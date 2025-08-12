47f2ff029f7ad27006f5747d9fb29b9161bd1e1c
Saurabh Sahani <saurabhsahani2710@gmail.com>
2025-08-12 19:29:10 +0530

    minor fix in FRONTEND_URL

--- src/server.js
+++ src/server.js
@@ -17,25 +17,12 @@ connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
-
-const allowedOrigins = [
-  process.env.FRONTEND_URL, // production
-  "http://localhost:5173"   // local dev
-];
-
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
   credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
-
-app.use(cors(corsOptions));
-
 
 app.use(cors(corsOptions));
 app.use("/api/user", userRoutes);
