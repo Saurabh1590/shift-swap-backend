0777b18e888e6a0bbc5202a92c63d14d7d8487e2
Saurabh Sahani <saurabhsahani2710@gmail.com>
2025-08-12 18:29:09 +0530

    minor fix

--- src/server.js
+++ src/server.js
@@ -17,14 +17,25 @@ connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
+
+const allowedOrigins = [
+  process.env.FRONTEND_URL, // production
+  "http://localhost:5173"   // local dev
+];
+
 const corsOptions = {
-  // origin: process.env.FRONTEND_URL || "http://localhost:5173",
-  // credentials: true,
-  // methods: ["GET", "POST", "PUT", "DELETE"],
-  // allowedHeaders: ["Content-Type", "Authorization"],
-  origin: process.env.FRONTEND_URL, 
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
 };
+
+app.use(cors(corsOptions));
+
 
 app.use(cors(corsOptions));
 app.use("/api/user", userRoutes);
